db194f7706ee23710bb81d0d2715468d25b7965b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-15 21:07:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,13 @@ class Song
   end
 
   def self.new_by_filename(filename)
-
+    #sample filename: 'Michael Jackson - Black or White - pop.mp3'
+    split = filename.split(" - ")
+    artist_name = split[0]
+    artist = Artist.find_or_create_by_name(artist_name)
+    song_name = split[1].chomp(".mp3")
+    song = Song.new(song_name)
+    song.artist = artist
   end
 
 end
